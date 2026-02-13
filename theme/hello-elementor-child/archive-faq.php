d13415266184167b65f134576136ba67d9aaa5ef
Ayrton Gonsallo <ayrtongonsallo444@gmail.com>
2026-02-13 12:59:57 +0100

    faq, css

--- theme/hello-elementor-child/archive-faq.php
+++ theme/hello-elementor-child/archive-faq.php
@@ -4,12 +4,20 @@
 
     <?php echo do_shortcode('[elementor-template id="10213"]'); ?>
     
-    <div class="faq-search-result-container">
+    <div class="faq-search-result-container" id="scroll">
         <!--  Barre de recherche -->
-        <div class="faq-search-wrapper" style="display:flex;justify-content:center;margin-bottom:40px;">
-            <input type="text" id="faq-search-input" placeholder="Rechercher une question..." style="padding:12px 16px; font-size:16px; width:300px;"/>
-            <button id="faq-search-btn" style="margin-left:10px; padding:12px 16px; cursor:pointer; font-size:16px;">
-                🔍
+        <div class="faq-search-wrapper" style="display:flex;justify-content:center;margin-bottom:60px;position: relative;width: 80%;margin-inline:auto;">
+            <input type="text" id="faq-search-input" placeholder="Que recherchez-vous ?" style="border: 1px solid rgba(227, 227, 227, 1) !important;border-radius: 50px 50px 50px 50px !important; background-color: rgba(227, 227, 227, 1); padding: 4px 35px !important; height: 40px;"/>
+            <button id="faq-search-btn" style="cursor: pointer;width: 28px;height: 40px;border-radius: 50px;display: flex;align-items: center;justify-content: center;background-color: #BFBFBF;border: none !important;position: absolute;right: 0;">
+            <span class="innericon" style="display:block; height: 20px;">
+			<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="22" height="22" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+					<path d="M460.355,421.59L353.844,315.078c20.041-27.553,31.885-61.437,31.885-98.037
+						C385.729,124.934,310.793,50,218.686,50C126.58,50,51.645,124.934,51.645,217.041c0,92.106,74.936,167.041,167.041,167.041
+						c34.912,0,67.352-10.773,94.184-29.158L419.945,462L460.355,421.59z M100.631,217.041c0-65.096,52.959-118.056,118.055-118.056
+						c65.098,0,118.057,52.959,118.057,118.056c0,65.096-52.959,118.056-118.057,118.056C153.59,335.097,100.631,282.137,100.631,217.041
+						z"></path>
+				</svg>
+		</span>
             </button>
         </div>
 
@@ -17,7 +25,7 @@
         <div id="faq-container" class="faq-grid">
             <?php query_posts([
     'post_type' => 'faq',
-    'orderby'   => 'title',
+    'orderby'   => 'menu_order',
     'order'     => 'ASC'
 ]);
             if ( have_posts() ) :
@@ -45,10 +53,7 @@
     </div>
 
 
-    <?php echo do_shortcode('[elementor-template id="3483"]'); ?>
-    <?php echo do_shortcode('[elementor-template id="4153"]'); ?>
-    <?php echo do_shortcode('[elementor-template id="3490"]'); ?>
-    <?php echo do_shortcode('[elementor-template id="3493"]'); ?>
+    <?php echo do_shortcode('[elementor-template id="10243"]'); ?>
 
 </main>
 
@@ -58,7 +63,7 @@
 <script>
     jQuery(function ($) {
 
-      
+        jQuery(function($){
 
     function loadMore(page, keyword = '') {
         $.ajax({
@@ -79,7 +84,7 @@
                     $('#faq-container').append(res); // ajout load more
                 }
 
-                $('#faq-load-more').text('En savoir plus');
+                $('#faq-load-more').text('Voir plus');
                 const maxPage = parseInt($('#faq-load-more').data('max'));
                 if(page >= maxPage || res.trim() == '') $('#faq-load-more').hide();
             },
@@ -109,7 +114,7 @@
         loadMore(nextPage, keyword);
     });
 
-
+});
 
 
     });
